308ab9f167eb7b41a099ca226e6a2148d770e1f2
Danna Bohnhoff <dannab101@gmail.com>
2022-12-23 15:28:10 -0500

    first error message

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 
+// app.set('view engine', 'jsx')
+// app.engine('jsx', require('express-react-views').createEngine())
+
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.send('Home')
 })
 
 app.get('*', (req, res) => {
-    res.render('error404')
+    res.status(404).send('<H1>error404</h1>')
 })
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, console.log (`listening on port ${PORT}`))
