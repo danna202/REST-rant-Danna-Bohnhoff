f770dc83715d76869500ab56e8a00bb1ef06ff0c
Danna Bohnhoff <dannab101@gmail.com>
2022-12-23 13:52:37 -0500

    places

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
+
+app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.listen(8080)
+app.get('*', (req, res) => {
+    res.render('error404')
+})
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, console.log (`listening on port ${PORT}`))
